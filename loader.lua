3f636f0c73f4234ba5926e01f9f88a751df21d76
Lunar-Eclipse111 <79114156+Lunar-Eclipse111@users.noreply.github.com>
2025-02-04 20:37:33 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,9 +2,15 @@ local g={
 Baseplate = 4483381587,
 }
 
-local function Render_Game(e)
+local function Game(e)
 e = e or {}
   if game.PlaceId == e.Game then
     loadstring(game:HttpGet(e.Link))()
   end
 end
+
+
+Game({
+	Game = g.Baseplate,
+	Link = "https://raw.githubusercontent.com/Lunar-Eclipse111/Swift.LOL/refs/heads/Scripts/Scripts/Universal/Dex"
+})
